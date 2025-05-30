9a416bb78cf0249d6588c37884f0f1dfdc0760b1
jaucaco <joakoballesteros2005@gmail.com>
2025-05-30 15:24:41 -0300

    guar

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, send_from_directory, render_template, redirect
+import os
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+# Ruta principal que sirve el menú
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Redirecciona a microservicio de Guardia
+@app.route("/guardia")
+def guardia():
+    return redirect("https://controlguardia.onrender.com")
+
+# Redirecciona a microservicio de Distribución
+@app.route("/distribucion")
+def distribucion():
+    return redirect("https://distribucion-1.onrender.com/")
+
+# Archivos estáticos (CSS, JS, imágenes)
+@app.route('/img/<path:filename>')
+def serve_images(filename):
+    return send_from_directory(os.path.join(app.static_folder, 'img'), filename)
+
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory(os.path.join(app.static_folder, 'css'), filename)
+
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory(os.path.join(app.static_folder, 'js'), filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
